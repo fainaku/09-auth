eed49af4df4a30a311589ae5d907744ba2be532d
Faina K <fayina1@gmail.com>
2025-11-01 21:24:50 +0100

    test middlewaew

--- app/(private routes)/notes/filter/[...slug]/page.tsx
+++ app/(private routes)/notes/filter/[...slug]/page.tsx
@@ -1,4 +1,3 @@
-import { fetchNotes } from "@/lib/api/clientApi";
 import {
   dehydrate,
   HydrationBoundary,
@@ -7,6 +6,7 @@ import {
 import NotesClient from "./Notes.client";
 import { NoteTag } from "@/types/note";
 import { Metadata } from "next";
+import { getServerNotes } from "@/lib/api/serverApi";
 
 interface NotesPageProps {
   params: Promise<{ slug: [NoteTag | "all"] }>;
@@ -46,7 +46,7 @@ const NotesPage = async ({ params }: NotesPageProps) => {
 
   await queryClient.prefetchQuery({
     queryKey: ["notes", tag],
-    queryFn: () => fetchNotes({ tag }),
+    queryFn: () => getServerNotes({ tag }),
   });
 
   return (
--- lib/api/serverApi.ts
+++ lib/api/serverApi.ts
@@ -1,6 +1,6 @@
 import { cookies } from "next/headers";
 import { nextServer } from "./api";
-import { User } from "./clientApi";
+import { FetchNotesResponse, User } from "./clientApi";
 import { Note } from "@/types/note";
 
 export const checkServerSession = async () => {
@@ -28,6 +28,31 @@ export const getServerNoteById = async (id: string): Promise<Note> => {
   const cookieStore = await cookies();
 
   const { data } = await nextServer.get<Note>(`/notes/${id}`, {
+    headers: {
+      Cookie: cookieStore.toString(),
+    },
+  });
+  return data;
+};
+
+export const getServerNotes = async ({
+  search,
+  page = 1,
+  perPage = 12,
+  tag,
+}: {
+  search?: string;
+  page?: number;
+  perPage?: number;
+  tag?: string;
+}): Promise<FetchNotesResponse> => {
+  const { data } = await nextServer.get<FetchNotesResponse>("/notes", {
+    params: {
+      search,
+      page,
+      perPage,
+      tag,
+    },
     headers: {
       Cookie: cookieStore.toString(),
     },
